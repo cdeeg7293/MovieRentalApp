bfe4b001ad9c1cfb82e915f849882a0328cefb80
Pavan Naga Teja <vemanapavan@gmail.com>
2014-02-20 15:58:37 +0530

    Customer Validations

--- app/models/customers.rb
+++ app/models/customers.rb
@@ -1,4 +1,7 @@
 class Customers < ActiveRecord::Base
+	validates :name, :presence => true, allow_blank: false
+	validates :mobile, :presence => true, allow_blank: false
+	validates :address, :presence => true, allow_blank: false
 	validates :movie_name, :presence => true, allow_blank: false
 	validates :mobile, length: { is: 10}
 end
